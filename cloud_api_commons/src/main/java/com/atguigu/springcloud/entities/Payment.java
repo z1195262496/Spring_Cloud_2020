e835fe2b7936ce667ba395607f20863e162c6b7d
gus_git <1195262496@qq.con>
2020-07-02 16:22:41 +0800

    p14 工程重构

---
+++ cloud_api_commons/src/main/java/com/atguigu/springcloud/entities/Payment.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * @Auther Augustus619
+ * @Date 2020-07-02 14:42
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Payment implements Serializable {
+    private Long id;
+    private String serial;
+
+}
