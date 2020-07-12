2498c42bc6e91f6b7f9a70e1ce51ae04eb6697ad
gus_git <1195262496@qq.con>
2020-07-12 20:04:02 +0800

    p30 ZK

---
+++ cloud-consumerzk-order80/src/main/java/com/atguigu/springcloud/OrderZKMain80.java
@@ -0,0 +1,15 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Auther Augustus619
+ * @Date 2020-07-12 19:54
+ */
+@SpringBootApplication
+public class OrderZKMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderZKMain80.class, args);
+    }
+}
---
+++ cloud-provider-payment8004/src/main/java/com/atguigu/springcloud/PaymentMain8004.java
@@ -0,0 +1,17 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Auther Augustus619
+ * @Date 2020-07-12 17:02
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain8004 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8004.class, args);
+    }
+}
